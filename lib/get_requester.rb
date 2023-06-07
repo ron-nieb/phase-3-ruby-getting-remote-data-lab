96483a5aaa605ad83bbe8f5e6b9316cf45b81d6b
Ronald Barasa <barasaogoro@gmail.com>
2023-06-07 18:44:47 +0300

    complete assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+
+class GetRequester
+    URL="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+    def initialize(url)
+      @url = url
+    end
+  
+    def get_response_body
+      uri = URI.parse(@url)
+      response = Net::HTTP.get_response(uri)
+      response.body
+    end
+  
+    def parse_json
+      response_body = get_response_body
+      JSON.parse(response_body)
+    end
+  end
